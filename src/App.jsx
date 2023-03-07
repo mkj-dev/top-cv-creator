19ca7015f0f10fe0249651fe15837a9d330e075d
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-07 19:54:20 +0100

    Import CVHeader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import './styles/App.css'
+import CVHeader from './components/CVHeader'
 
 function App() {
 
   return (
     <div className="App">
+      <CVHeader />
     </div>
   )
 }
