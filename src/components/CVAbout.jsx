0ff23c2cebdd11e6fb848342097fd5fb2dccb316
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-09 13:46:24 +0100

    Create CVAbout component with placeholder data

---
+++ src/components/CVAbout.jsx
@@ -0,0 +1,8 @@
+export default function CVAbout() {
+  return (
+    <>
+        <h3 className="about-title">About Me</h3>
+        <p className="about-para">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet distinctio officia repudiandae, soluta error nostrum atque similique mollitia in ullam. Nobis numquam nostrum placeat reprehenderit, esse quisquam cumque officiis, distinctio doloribus molestiae rem ullam quod error, reiciendis pariatur? Vero excepturi voluptate id nihil corporis reiciendis.</p>
+    </>
+  );
+}
