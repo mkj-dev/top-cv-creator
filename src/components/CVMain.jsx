4942705859cd465c6cc68dde607420c7fee7e04d
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-09 16:11:13 +0100

    Change id and class names

--- src/components/CVMain.jsx
+++ src/components/CVMain.jsx
@@ -4,11 +4,11 @@ import CVAside from './CVAside';
 
 export default function CVMain() {
   return (
-    <main id="cv-main">
-      <section className='section-main'>
+    <main id="cv-document">
+      <section className='cv-main-section'>
         <CVAbout />
       </section>
-      <section className='section-aside'>
+      <section className='cv-aside-section'>
         <CVAside />
       </section>
     </main>
