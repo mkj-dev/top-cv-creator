1c6e26d05ef370daf0a21556761fadb44662eb3d
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-10 13:44:23 +0100

    Import and add CVExperience component

--- src/components/CVMain.jsx
+++ src/components/CVMain.jsx
@@ -1,12 +1,14 @@
 import '../styles/CVMain.css';
 import CVAbout from './CVAbout';
 import CVAside from './CVAside';
+import CVExperience from './CVExperience';
 
 export default function CVMain() {
   return (
     <main id="cv-document">
       <section className='cv-main-section'>
         <CVAbout />
+        <CVExperience />
       </section>
       <section className='cv-aside-section'>
         <CVAside />
