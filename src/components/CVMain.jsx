54ac67de62902824387e447a834247533910937a
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-13 15:02:34 +0100

    Change import paths

--- src/components/CVMain.jsx
+++ src/components/CVMain.jsx
@@ -1,8 +1,8 @@
 import '../styles/CVMain.css';
-import CVAbout from './CVAbout';
-import CVAside from './CVAside';
-import CVEducation from './CVEducation';
-import CVExperience from './CVExperience';
+import CVAbout from './cv_main_section/CVAbout';
+import CVAside from './cv_aside_section/CVAside';
+import CVEducation from './cv_main_section/CVEducation';
+import CVExperience from './cv_main_section/CVExperience';
 
 export default function CVMain() {
   return (
