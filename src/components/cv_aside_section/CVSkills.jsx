aa48e72eea7f9ceac2542cf97cefc40ab55c4c12
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-14 09:43:05 +0100

    Create handleNewSkill() and addSkill() functions that handle the addition of a new skill

--- src/components/cv_aside_section/CVSkills.jsx
+++ src/components/cv_aside_section/CVSkills.jsx
@@ -3,39 +3,35 @@ import { useState } from 'react';
 export default function CVSkills() {
   const [isEditable, setIsEditable] = useState(false);
   const [skills, setSkills] = useState(['Skill 1', 'Skill 2']);
+  const [skillName, setSkillName] = useState('');
 
   const handleEditToggle = () => {
     setIsEditable(!isEditable);
   }
 
-  const handleAddSkill = () => {
-    setSkills([...skills, '']);
-  };
+  function handleNewSkill(event) {
+    setSkillName(event);
+  }
 
-  const handleSkillChange = (index, value) => {
-    const newSkills = [...skills];
-    newSkills[index] = value;
-    setSkills(newSkills);
-  };
+  const addSkill = (skill) => {
+    const newSkill = skill;
+    setSkills([...skills, newSkill]);
+  }
 
   return (
     <section id='skills-section'>
       <h3 className='skills-title'>Skills</h3>
-      {!isEditable && (
-        <>
-          <ul>
-            {skills.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
-          {/* <input
-                  type='text'
-                  value={skill}
-                  onChange={(event) => handleSkillChange(index, event.target.value)}
-                /> 
-            */}
-          <button onClick={handleAddSkill}>Add Skill</button>
-        </>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+      {isEditable && (
+        <form action="">
+          <label htmlFor="new-skill">New Skill:</label>
+          <input type="text" id='new-skill' name='new-skill' onChange={e => handleNewSkill(e.target.value)} />
+          <button type="button" className='add-button' onClick={() => addSkill(skillName)}>Add skill</button>
+        </form>
       )}
       <button className='edit-button' onClick={handleEditToggle}>{isEditable ? 'Save' : 'Edit'}</button>
     </section>
