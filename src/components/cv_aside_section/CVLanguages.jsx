f5257826bcd957b7811258b109137d9ac97aaaa1
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-14 11:45:27 +0100

    Create handleNewLanguage() and addNewLanguage() functions that handle the addition of a new language skill

--- src/components/cv_aside_section/CVLanguages.jsx
+++ src/components/cv_aside_section/CVLanguages.jsx
@@ -1,11 +1,41 @@
+import { useState, useRef } from "react";
+
 export default function CVLanguages() {
+  const [isEditable, setIsEditable] = useState(false);
+  const [languageSkills, setLanguageSkills] = useState(['English - B2', 'Spanish - A1']);
+  const newLanguageRef = useRef(null);
+
+  const handleEditToggle = () => {
+    setIsEditable(!isEditable);
+  }
+
+  const handleNewLanguage = (event) => {
+    newLanguageRef.current = event.target.value;
+  }
+
+  const addNewLanguage = (event) => {
+    event.preventDefault();
+    const newLanguage = newLanguageRef.current.trim();
+    setLanguageSkills([...languageSkills, newLanguage]);
+    newLanguageRef.current = '';
+  }
+
   return (
-    <section id='section-languages'>
+    <section id='languages-section'>
       <h3 className='languages-title'>Languages</h3>
       <ul>
-        <li>German - B1</li>
-        <li>Spanish - B2</li>
+        {languageSkills.map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
       </ul>
+      {isEditable && (
+        <form onSubmit={addNewLanguage}>
+          <label htmlFor="new-language">New language:</label>
+          <input type="text" id='new-language' name='new-language' onChange={handleNewLanguage} />
+          <button type="submit" className='add-button'>Add language</button>
+        </form>
+      )}
+      <button className='edit-button' onClick={handleEditToggle}>{isEditable ? 'Save' : 'Edit'}</button>
     </section>
-  )
+  );
 }
