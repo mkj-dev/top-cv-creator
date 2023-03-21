1903300a6a97656a2760fde71c05a25eb7daff20
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-21 12:45:01 +0100

    Change section id to className, add form labels

--- src/components/cv_aside_section/CVContact.jsx
+++ src/components/cv_aside_section/CVContact.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function CVContact() {
+export default function CVContact({ inputError, onInputErrorChange }) {
+
   const [isEditable, setIsEditable] = useState(false);
   const [contactInfo, setContactInfo] = useState({
-    mobile: '123456789',
-    email: 'your@email.com',
-    linkedin: 'https://www.linkedin.com/in/profile',
+    mobile: "123456789",
+    email: "your@email.com",
+    linkedin: "https://www.linkedin.com/in/profile",
   });
+
+  const handleEditToggle = () => {
+    setIsEditable(!isEditable);
+  }
 
   const handleContactInfoChange = (event, field) => {
     const updatedContactInfo = { ...contactInfo };
@@ -14,46 +19,62 @@ export default function CVContact() {
     setContactInfo(updatedContactInfo);
   }
 
-  const handleEditToggle = () => {
-    setIsEditable(!isEditable);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (contactInfo.mobile.trim() === "" || contactInfo.email.trim() === "" || contactInfo.linkedin.trim() === "") {
+      onInputErrorChange("Add proper contact info.");
+      return;
+    }
+
+    onInputErrorChange("");
+    console.log("Submitting contact info:", contactInfo);
   }
 
   return (
-    <section id='contact-section'>
-      <h3 className='contact-title'>Contact</h3>
+    <section className="contact-section">
+      <h3 className="contact-title">Contact</h3>
       {!isEditable && (
         <>
           <p>Mobile: {contactInfo.mobile}</p>
           <p>Email: {contactInfo.email}</p>
-          <a href={contactInfo.linkedin} target='_blank'>LinkedIn Profile</a>
+          <a href={contactInfo.linkedin} target="_blank">LinkedIn Profile</a>
         </>
       )}
       {isEditable && (
-        <>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="mobile">Mobile:</label>
           <input
-            type='tel'
-            placeholder='Mobile'
+            type="tel"
+            placeholder="Mobile"
+            id="mobile"
             value={contactInfo.mobile}
-            onChange={(event) => handleContactInfoChange(event, 'mobile')}
+            onChange={(event) => handleContactInfoChange(event, "mobile")}
             required
           />
+          <label htmlFor="email">Email:</label>
           <input
-            type='email'
-            placeholder='Email'
+            type="email"
+            placeholder="Email"
+            id="email"
             value={contactInfo.email}
-            onChange={(event) => handleContactInfoChange(event, 'email')}
+            onChange={(event) => handleContactInfoChange(event, "email")}
             required
           />
+          <label htmlFor="linkedin">LinkedIn:</label>
           <input
-            type='url'
-            placeholder='LinkedIn profile link'
+            type="url"
+            placeholder="LinkedIn profile link"
+            id="linkedin"
             value={contactInfo.linkedin}
-            onChange={(event) => handleContactInfoChange(event, 'linkedin')}
+            onChange={(event) => handleContactInfoChange(event, "linkedin")}
             required
           />
-        </>
+          {inputError && <p className="error">{inputError}</p>}
+          <button type="submit" className="add-button">Add contact info</button>
+        </form>
       )}
-      <button className='edit-button' onClick={handleEditToggle}>{isEditable ? 'Save' : 'Edit'}</button>
+      <button className="edit-button" onClick={handleEditToggle}>{isEditable ? "Save" : "Edit"}</button>
     </section>
   );
 }
