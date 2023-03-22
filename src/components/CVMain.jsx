4c6c5aff6df7b731c12ef0023b4e37339e05f3fd
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-22 17:58:32 +0100

    Move ToggleButton component to the main.jsx

--- src/components/CVMain.jsx
+++ src/components/CVMain.jsx
@@ -3,22 +3,18 @@ import CVAbout from './cv_main_section/CVAbout';
 import CVAside from './cv_aside_section/CVAside';
 import CVEducation from './cv_main_section/CVEducation';
 import CVExperience from './cv_main_section/CVExperience';
-import ToggleButton from './ToggleButton';
 
 export default function CVMain() {
   return (
-    <>
-      <main id="cv-document">
-        <section className='cv-main-section'>
-          <CVAbout />
-          <CVExperience />
-          <CVEducation />
-        </section>
-        <section className='cv-aside-section'>
-          <CVAside />
-        </section>
-      </main>
-      <ToggleButton />
-    </>
+    <main id="cv-document">
+      <section className='cv-main-section'>
+        <CVAbout />
+        <CVExperience />
+        <CVEducation />
+      </section>
+      <section className='cv-aside-section'>
+        <CVAside />
+      </section>
+    </main>
   );
 }
