2f2af678e7df61a7e79c3cbf62243dff9b565f30
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-22 17:59:14 +0100

    Import and add ToggleButton component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import ToggleButton from './components/ToggleButton';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
+		<ToggleButton />
 	</React.StrictMode>,
 )
