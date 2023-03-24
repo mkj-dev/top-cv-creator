de05bd794f9a8ff019d3a55e5b5df2545167e8ab
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-03-24 19:51:43 +0100

    Import and add DownloadPdfButton component, create #button-container div

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DownloadPdfButton from './components/DownloadPdfButton';
 import ToggleButton from './components/ToggleButton';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
-		<ToggleButton />
+		<div id='button-container'>
+			<ToggleButton />
+			<DownloadPdfButton />
+		</div>
 	</React.StrictMode>,
 )
